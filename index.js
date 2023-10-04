dee7472a4f2a0c74c9d4949056f7eb442c90af6d
nicolecraven <craventhegreat@gmail.com>
2023-10-03 20:07:40 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solution in this file!
+
+const hqLocation = 42;
+function distanceFromHqInBlocks (customerLocation){
+return Math.abs(customerLocation - hqLocation);
+}
+
+console.log(distanceFromHqInBlocks(43))
+
+//const hqLocation = 42;
+function distanceFromHqInFeet(customerLocation){
+    return Math.abs( customerLocation - hqLocation) * 264;
+}
+
+//function distanceTravelledInFeet(block, feet){
+    //return Math.abs(block *feet);
+//}
+
+
+
+function distanceTravelledInFeet(start, destination){
+return Math.abs(start - destination) * 264;
+}
+console.log(distanceTravelledInFeet(8, 10))
+
+
+function calculatesFarePrice(start, destination){
+   let feet = Math.abs(start - destination) * 264;
+     if (feet <= 400){
+        return 0
+    }
+    if (feet > 400 && feet <=2000){
+    const price = (feet - 400)*.02;
+    return price
+
+    }
+    if (feet >= 2000){
+        return 25
+    }
+    if (feet <= 2500){
+     return "Cannot travel that far";
+        
+    }
+
+
+
+}
+console.log(calculatesFarePrice(34,43))
+
+
+
+
+
+
